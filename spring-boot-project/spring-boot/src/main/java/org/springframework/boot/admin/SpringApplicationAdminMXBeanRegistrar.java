3faf7c04665f647bec339460071c598c2ed8c2f8
deerjie <52825730+deerjie@users.noreply.github.com>
2020-01-06 22:49:21 +0800

    Update SpringApplicationAdminMXBeanRegistrar.java

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/admin/SpringApplicationAdminMXBeanRegistrar.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/admin/SpringApplicationAdminMXBeanRegistrar.java
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ */kjkljlkj
 
 package org.springframework.boot.admin;
 
@@ -87,8 +87,7 @@ public class SpringApplicationAdminMXBeanRegistrar implements ApplicationContext
 		if (type == null) {
 			return false;
 		}
-		return ApplicationReadyEvent.class.isAssignableFrom(type)
-				|| WebServerInitializedEvent.class.isAssignableFrom(type);
+		return ApplicationReadyEvent.class.isAssignableFrom(type)|| WebServerInitializedEvent.class.isAssignableFrom(type);
 	}
 
 	@Override
